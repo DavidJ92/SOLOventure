268e48906d75a63c16235494c60a321973fbb8a9
Za Matheson <zamatheson@Zas-MacBook-Air.local>
2024-02-29 13:01:51 -0600

    css styling and logic/functionality. not completely done, still some errors

--- client/App.js
+++ client/App.js
@@ -1,25 +1,20 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Journal from './pages/Journal';
-import Communities from './pages/Communities';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-
+import Journal from '../pages/Journal';
+import Communities from '../pages/Communities';
+import NavBar from '../components/NavBar';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/communities" component={Communities} />
         <Route path="/journal" component={Journal} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/communities" component={Communities} />
       </Switch>
     </Router>
   );
 }
 
 export default App;
+
